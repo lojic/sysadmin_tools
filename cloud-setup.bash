0d3f9c9a978c31ece293a380bf92be8d63f0017d
Brian Adkins <info@lojic.com>
2011-05-28 13:46:43 -0400

    The setup script is functionally complete.

--- cloud-setup.bash
+++ cloud-setup.bash
@@ -1,41 +1,57 @@
 #!/bin/bash
 
-# Setup an Ubuntu VPS
+# This Bash script sets up a new Ubuntu server.
+# Copyright 2011 Brian Adkins, Lojic Technologies, LLC
 
 #------------------------------------------------------------
 # Modify values below here
 #------------------------------------------------------------
 
-CHKROOTKIT=1
-ECHO_COMMANDS=0
-EMACS=1
+USERNAME=deploy
+NOTIFICATION_EMAIL=  # fail2ban will send emails to this address
 
-GEM_CREDITCARD=1
-GEM_JSON=1
-GEM_WILL_PAGINATE=1
+# To install postfix, specify a non-empty domain name for POSTFIX_DOMAIN
+POSTFIX_DOMAIN=
+
+# To setup ssh keys, specify a url containing a public key
+PUBLIC_KEY_URL=
+
+
+# Boolean flags 1 => true, 0 => false
+CHKROOTKIT=1             # Install chkrootkit root kit checker via apt-get
+ECHO_COMMANDS=0          # Echo commands from script
+EMACS=1                  # Install Emacs via apt-get
+FAIL2BAN=1               # Install fail2ban via apt-get
+GEM_CREDITCARD=1         # Ruby gem: creditcard
+GEM_JSON=1               # Ruby gem: json
+GEM_WILL_PAGINATE=1      # Ruby gem: will_paginate
+GHC=1                    # Install Glasgow Haskell Compiler via apt-get
+MLTON=1                  # Install MLton Standard ML Compiler via apt-get
+PASSENGER=1              # Install Phusion Passenger and nginx
+POSTGRES=1               # Install Postgres database via apt-get
+RKHUNTER=1               # Install rkhunter root kit checker via apt-get
+SCREEN=1                 # Install screen via apt-get
+SHOREWALL=1              # Install shorewall firewall via apt-get
+
+# Prevent prompts during postfix installation
+export DEBIAN_FRONTEND=noninteractive
 
 # To install memcached, specify a RAM amount > 0 e.g. 16
 MEMCACHED_RAM=16
 
-PASSENGER=1
-POSTFIX=1
-POSTGRES=1
-PUBLIC_KEY_URL=http://lojic.com/id_rsa.pub
-
 # To install Rails, set RAILS_VERSION to a non-empty version string
 RAILS_VERSION=3.0.7
 
-RKHUNTER=1
+# To install Ruby, specify a url for source
 RUBY_SOURCE=http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
-SCREEN=1
-SHOREWALL=1
+
+# To install Trust Commerce's tclink API, specify a url for source
+TCLINK_SOURCE=https://vault.trustcommerce.com/downloads/tclink-3.4.4-ruby.tar.gz
 
 # To install thttpd, specify a port > 0 e.g. 8000
-THTTPD_PORT=8000
+THTTPD_PORT=0
 
 TIMEZONE=US/Eastern
-USERNAME=badkins1
-UPDATE_UBUNTU=1
 WWW_DIR=/var/www
 
 #------------------------------------------------------------
@@ -111,17 +127,15 @@ function apt_get_packages() {
   if [ "$MEMCACHED_RAM" -gt 0 ]; then
     display_message "Installing memcached"
     apt-get -y install memcached
-    sed -i -e "/^-m 64/c-m ${MEMCACHED_RAM}" /etc/memcached.conf
+    sed -i.orig -e "/^-m 64/c-m ${MEMCACHED_RAM}" /etc/memcached.conf
   fi
   
-  if [ "$POSTFIX" = 1 ]; then
-    display_message "Installing postfix"
-    apt-get -y install postfix
+  if [ "$POSTFIX_DOMAIN" ]; then
+    install_postfix
   fi
   
   if [ "$POSTGRES" = 1 ]; then
-    display_message "Installing postgresql"
-    apt-get -y install libecpg-dev postgresql
+    install_postgres
   fi
   
   if [ "$THTTPD_PORT" -gt 0 ]; then
@@ -133,6 +147,104 @@ function apt_get_packages() {
     apt-get -y install libcurl4-openssl-dev
   fi
   
+  if [ "$MLTON" = 1 ]; then
+    display_message "Installing mlton"
+    apt-get -y install mlton
+  fi
+  
+  if [ "$FAIL2BAN" = 1 ]; then
+    install_fail2ban
+  fi
+  
+}
+
+function configure_logrotate() {
+  cat >> /etc/logrotate.conf <<'EOF'
+/usr/local/nginx/logs/*.log {
+    missingok
+    notifempty
+    sharedscripts
+    postrotate
+        test ! -f /usr/local/nginx/logs/nginx.pid || kill -USR1 `cat /usr/local/nginx/logs/nginx.pid`
+    endscript
+}
+EOF
+}
+
+function configure_nginx() {
+  cat > /etc/init.d/nginx <<'EOF'
+# Description: Startup script for nginx webserver on Debian. Place in /etc/init.d and 
+# run 'sudo update-rc.d nginx defaults', or use the appropriate command on your
+# distro.
+#
+# Author:       Ryan Norbauer <ryan.norbauer@gmail.com>
+# Modified:     Geoffrey Grosenbach http://topfunky.com
+
+set -e
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DESC="nginx daemon"
+NAME=nginx
+DAEMON=/usr/local/nginx/sbin/$NAME
+CONFIGFILE=/usr/local/nginx/conf/nginx.conf
+#PIDFILE=/var/run/$NAME.pid
+PIDFILE=/usr/local/nginx/logs/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+
+# Gracefully exit if the package has been removed.
+test -x $DAEMON || exit 0
+
+d_start() {
+  $DAEMON -c $CONFIGFILE || echo -n " already running"
+}
+
+d_stop() {
+  kill -QUIT `cat $PIDFILE` || echo -n " not running"
+}
+
+d_reload() {
+  kill -HUP `cat $PIDFILE` || echo -n " can't reload"
+}
+
+case "$1" in
+  start)
+        echo -n "Starting $DESC: $NAME"
+        d_start
+        echo "."
+        ;;
+  stop)
+        echo -n "Stopping $DESC: $NAME"
+        d_stop
+        echo "."
+        ;;
+  reload)
+echo -n "Reloading $DESC configuration..."
+d_reload
+        echo "reloaded."
+  ;;
+  restart)
+        echo -n "Restarting $DESC: $NAME"
+        d_stop
+        # One second might not be time enough for a daemon to stop, 
+        # if this happens, d_start will fail (and dpkg will break if 
+        # the package is being upgraded). Change the timeout if needed
+        # be, or change d_stop to have start-stop-daemon use --retry. 
+        # Notice that using --retry slows down the shutdown process somewhat.
+        sleep 1
+        d_start
+        echo "."
+        ;;
+  *)
+          echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
+          exit 3
+        ;;
+esac
+
+exit 0
+EOF
+  chmod 755 /etc/init.d/nginx
+  display_message "Setting update-rc.d defaults for nginx"
+  update-rc.d nginx defaults  
 }
 
 function configure_timezone() {
@@ -153,9 +265,10 @@ function create_user() {
   adduser $username
 
   # Add user to sudoer lsit
-  sed -i "\$a$username ALL=ALL" /etc/sudoers
+  sed -i.orig "\$a$username ALL=ALL" /etc/sudoers
 
   # Configure command history
+  cp /home/${username}/.bashrc /home/${username}/.bashrc.orig
   sed -i -e '/^HISTSIZE/cHISTSIZE=8192' /home/${username}/.bashrc
   sed -i -e '/^HISTFILESIZE/cHISTFILESIZE=8192' /home/${username}/.bashrc
   sed -i -e '/^HISTCONTROL=ignoredups:ignorespace/cHISTCONTROL=ignoredups:ignorespace:erasedups' /home/${username}/.bashrc
@@ -203,6 +316,19 @@ function initialize() {
   THTTPD_PORT=${THTTPD_PORT:-0}
 }
 
+function install_fail2ban() {
+  display_message "Installing fail2ban"
+  apt-get -y install fail2ban
+  cp /etc/fail2ban//jail.conf /etc/fail2ban/jail.local
+  sed -i -e '/^bantime/cbantime = 1800' /etc/fail2ban/jail.local
+
+  if [ "$NOTIFICATION_EMAIL" ]; then
+    sed -i -e "/^destemail/cdestemail = ${NOTIFICATION_EMAIL}" /etc/fail2ban/jail.local
+  fi
+  
+  /etc/init.d/fail2ban restart
+}
+
 function install_gems() {
   if [ "$POSTGRES" = 1 ]; then
     display_message "Installing pg gem"
@@ -229,13 +355,32 @@ function install_passenger() {
   display_message "Installing passenger"
   gem install --no-rdoc --no-ri passenger
   passenger-install-nginx-module --auto --auto-download --prefix=/usr/local/nginx
+  configure_nginx
+}
+
+function install_postfix() {
+  display_message "Installing postfix"
+  apt-get -y install postfix
+  display_message "Modifying /etc/postfix/main.cf"
+  cp /etc/postfix/main.cf /etc/postfix/main.cf.orig
+  sed -i -e "/^myhostname/cmyhostname = ${POSTFIX_DOMAIN}" /etc/postfix/main.cf
+  sed -i -e "/^mydestination/cmydestination = localhost.localdomain, localhost" /etc/postfix/main.cf
+  sed -i -e "/^inet_interfaces/cinet_interfaces = loopback-only" /etc/postfix/main.cf
+}
+
+function install_postgres() {
+  display_message "Installing postgresql"
+  apt-get -y install libecpg-dev postgresql
+  su -c psql postgres <<EOF
+create role $USERNAME superuser login;
+EOF
 }
 
 function install_rails() {
   local rails_version=$1
 
   if [ $rails_version ]; then
-    display_message "Installing Rails ${rails_version}"
+    display_message "Installing Rails ${rails_version} - this will take a while"
     gem install --no-rdoc --no-ri -v ${rails_version} rails
   fi
 }
@@ -312,7 +457,68 @@ fw      firewall
 net     ipv4
 EOF
 
-  sed -i -e '/^startup=/cstartup=1' /etc/default/shorewall
+  sed -i.orig -e '/^startup=/cstartup=1' /etc/default/shorewall
+}
+
+function install_tclink() {
+  local tclink_src=$1
+  
+  if [ $tclink_src ]; then
+    pushd /usr/local/src
+    display_message "Downloading tclink"
+    wget $tclink_src
+    local fname=$(file_name_from_path $tclink_src)
+    display_message "Extracting tclink"
+    tar xzf $fname
+    local prefix=${fname%\.tar\.gz}
+    cd $prefix
+    display_message "Patching tclink"
+    # Patch rb_tclink.c & tctest.rb
+    cat > patch.txt <<EOF
+diff --git a/rb_tclink.c b/rb_tclink.c
+index 1443c15..890b09f 100644
+--- a/rb_tclink.c
++++ b/rb_tclink.c
+@@ -48,8 +48,8 @@ tclink_send(VALUE obj, VALUE params) {
+ 		input_key = rb_funcall(input_keys, rb_intern("[]"), 1,
+                                        INT2FIX(i));
+ 		input_value = rb_hash_aref(params, input_key);
+-		TCLinkPushParam(handle, RSTRING(StringValue(input_key))->ptr,
+-                                RSTRING(StringValue(input_value))->ptr);
++		TCLinkPushParam(handle, RSTRING_PTR(StringValue(input_key)),
++                                RSTRING_PTR(StringValue(input_value)));
+ 	}
+ 
+ 	/* send the transaction */
+diff --git a/tctest.rb b/tctest.rb
+index 27c640e..df7b6f2 100755
+--- a/tctest.rb
++++ b/tctest.rb
+@@ -8,7 +8,7 @@ begin
+   require 'tclink'
+ rescue LoadError
+   print "Failed to load TCLink extension\n"
+-  exit
++  exit 1
+ end
+ 
+ print "TCLink version " + TCLink.getVersion() + "\n"
+@@ -35,3 +35,5 @@ print "done!\n\nTransaction results:\n"
+ for key in result.keys()
+   print "\t" + key + "=" + result[key] + "\n"
+ end
++
++exit 1 unless result['status'] == 'approved'
+EOF
+    patch <patch.txt
+    display_message "Building tclink"
+    ./build.sh
+    display_message "Copying to Ruby extensions directory"
+    cp tclink.so /usr/local/lib/ruby/1.9.1/x86_64-linux/
+    display_message "Testing tclink"
+    ruby tctest.rb
+    popd
+  fi
 }
 
 function install_thttpd() {
@@ -329,48 +535,34 @@ cgipat=**.rb
 throttles=/etc/thttpd/throttle.conf
 logfile=/var/log/thttpd.log
 EOF
-  sed -i -e '/^ENABLED=/cENABLED=yes' /etc/default/thttpd
+  sed -i.orig -e '/^ENABLED=/cENABLED=yes' /etc/default/thttpd
   popd
 }
 
-function prologue() {
+function epilogue() {
   if [ "$SHOREWALL" = 1 ]; then
-    echo "Test the firewall via: shorewall safe-start (and verify ssh)"
-  fi
-
-  if [ $MEMCACHED_RAM -gt 0 ]; then
     cat <<EOF
 
-Configure memcached via:
-sudo vim /etc/memcached.conf
+Test the firewall via: shorewall safe-start (and verify ssh)
 EOF
   fi
-  
-  if [ "$POSTFIX" = 1 ]; then
-    cat <<EOF
+  cat <<EOF
 
-vim /etc/postfix/main.cf
- * remove foo.com from mydestination to not deliver locally
- * change: myhostname = foo.com
- * change: inet_interfaces = loopback-only
-/etc/init.d/postfix restart
-EOF
-  fi
-  
-  if [ "$POSTGRES" = 1 ]; then
-    cat <<EOF
+Root has been disabled from logging in via ssh.
+Use the new user $USERNAME in conjunction with sudo.
 
-Configure postgresql:
-su postgres
-psql
-create role ${USERNAME} superuser login;
+Password login has been disabled, you must have your public
+key installed in ~/.ssh/authorized_keys
+
+Reboot is recommended to verify processes start properly at boot
 EOF
-  fi
 }
 
 # Prevent root login
 # Prevent password login
 function secure_ssh() {
+  cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+
   # Ensure root can't ssh in
   sed -i -e '/PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
 
@@ -382,11 +574,9 @@ function secure_ssh() {
 }
 
 function update_ubuntu() {
-  if [ "$UPDATE_UBUNTU" = 1 ]; then
-    display_message "Updating Ubuntu"
-    apt-get -y update
-    apt-get -y upgrade
-  fi
+  display_message "Updating Ubuntu"
+  apt-get -y update
+  apt-get -y upgrade
 }
 
 #------------------------------------------------------------
@@ -395,7 +585,7 @@ function update_ubuntu() {
 
 initialize
 configure_timezone $TIMEZONE
-echo 'Changing root password:'
+display_message 'Changing root password:'
 passwd # Change root passwd
 create_user $USERNAME $PUBLIC_KEY_URL
 secure_ssh
@@ -405,4 +595,5 @@ install_ruby $RUBY_SOURCE
 install_rails $RAILS_VERSION
 install_passenger
 install_gems
-prologue
+install_tclink $TCLINK_SOURCE
+epilogue
