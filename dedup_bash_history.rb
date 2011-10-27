a8894745d15cb6d1428a4020d6fdb7d869788075
Brian Adkins <info@lojic.com>
2011-10-26 22:57:27 -0400

    Add Ruby program to dedupe .bash_history file

---
+++ dedup_bash_history.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+# NOTE: Run from home directory
+#
+# This program will eliminate duplicate lines in .bash_history and
+# keep the last of any duplicates. For example:
+#
+# Before:
+#
+# ls
+# echo "hello"
+# ls
+# cat foo.txt
+# cat foo.txt
+# ls
+# echo "hello
+#
+# After:
+#
+# cat foo.txt
+# ls
+# echo "hello
+
+BACKUP_FILE  = '.bash_history_backup'
+HISTORY_FILE = '.bash_history'
+TEMP_FILE    = '.bash_history_temp'
+
+`cp #{HISTORY_FILE} #{BACKUP_FILE}`
+
+File.open(HISTORY_FILE, "r") do |hist_file|
+  File.open(TEMP_FILE, "w") do |temp_file|
+    hist_file.readlines.reverse.uniq.reverse.each {|line| temp_file.puts line }
+  end
+end
+
+# Replace history file with deduped file
+`mv #{TEMP_FILE} #{HISTORY_FILE}`
+
