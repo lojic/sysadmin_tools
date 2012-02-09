cbfd5d4345252ce5521b823b7fb95ba28d89b678
Tom Davies <tdavies@federatedmedia.net>
2012-02-09 16:34:17 -0500

    Updated bash history to add comment on increasing history size and so the history grabs from the user's home dir.

--- dedup_bash_history.rb
+++ dedup_bash_history.rb
@@ -2,7 +2,13 @@
 
 # Copyright (C) 2011 by Brian J. Adkins
 #
-# NOTE: Run from home directory
+# TIP: Add the following lines to your .bash_profile (or equiv)
+#      to save the last 10000 lines
+#
+# export HISTCONTROL=ignoredupes:erasedups
+# export HISTFILESIZE=10000
+# export HISTSIZE=10000
+
 #
 # This program will eliminate duplicate lines in .bash_history and
 # keep the last of any duplicates. For example:
@@ -23,17 +29,21 @@
 # ls
 # echo "hello
 
-BACKUP_FILE  = '.bash_history_backup'
-HISTORY_FILE = '.bash_history'
-TEMP_FILE    = '.bash_history_temp'
+BACKUP_FILE  = '~/.bash_history_backup'
+HISTORY_FILE = '~/.bash_history'
+TEMP_FILE    = '~/.bash_history_temp'
 
-`cp #{HISTORY_FILE} #{BACKUP_FILE}`
+history_file = File.expand_path(HISTORY_FILE)
+backup_file = File.expand_path(BACKUP_FILE)
+temp_file = File.expand_path(TEMP_FILE)
 
-File.open(HISTORY_FILE, "r") do |hist_file|
-  File.open(TEMP_FILE, "w") do |temp_file|
-    hist_file.readlines.reverse.uniq.reverse.each {|line| temp_file.puts(line) }
+`cp #{history_file} #{backup_file}`
+
+File.open(history_file, "r") do |hfile|
+  File.open(temp_file, "w") do |tfile|
+    hfile.readlines.reverse.uniq.reverse.each {|line| tfile.puts(line) }
   end
 end
 
-`mv #{TEMP_FILE} #{HISTORY_FILE}`
+`mv #{temp_file} #{history_file}`
 
